13bf36a5b1d73f16b22799f4efdfe43175598e6d
Yaseen Saad Eldin <yaseensaadeldinmohamed@gmail.com>
2024-12-14 11:41:59 +0200

    correcting vercel

--- api/index.js
+++ api/index.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, './views'));
+app.set('views', path.join(__dirname, '../views'));
 app.use(express.static(path.join(__dirname, '../public')));
 
 // CORS Configuration
